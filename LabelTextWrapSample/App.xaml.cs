94045feb91fbe1a231da8d336a2f3d559c8687f6
jimm <Jim@two-bits.net>
2023-06-09 11:25:31 -0700

    Add sample.

---
+++ LabelTextWrapSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LabelTextWrapSample;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ LabelTextWrapSample/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace LabelTextWrapSample;
+
+public partial class MainPage : ContentPage
+{
+    const string lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.";
+
+    int current = 1;
+
+    public MainPage() {
+        InitializeComponent();
+        SetLorem(current);
+    }
+
+
+    void ToolbarItem_Clicked(object sender, EventArgs e) {
+        SetLorem(++current);
+        current = current % 3;
+    }
+
+    void SetLorem(int count) {
+        string text = string.Join(' ', Enumerable.Repeat(lorem, count));
+        TextLabelOne.Text = TextLabelTwo.Text = TextLabelThree.Text = TextLabelFour.Text = text;
+    }
+}
